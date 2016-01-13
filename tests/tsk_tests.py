ca3f4d384fea740e2d44cc98a829b069d61ede03
Richard Klees <richard.klees@rwth-aachen.de>
2016-01-13 21:36:17 +0100

    log for testing.

--- tests/tsk_tests.py
+++ tests/tsk_tests.py
@@ -1,11 +1,21 @@
 from nose.tools import *
 import blg
 
+_log = [[]]
+
+def log(msg = None):
+    if msg is None:
+        return _log[0]
+    _log[0].append(msg)
+
 def setup():
-    print "SET UP!"
+    _log[0] = []
 
 def teardown():
-    print "TEAR DOWN!"
+    pass
 
-def test_basic():
-    print "I RAN!"
+def test_log():
+    log("foo")
+    log("bar")
+
+    assert log() == ["foo", "bar"]
