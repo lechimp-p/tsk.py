90429b0cee3ce46247e644c83ac87736c41dfc31
Richard Klees <richard.klees@rwth-aachen.de>
2016-01-20 20:42:27 +0100

    added node in *.py files

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
+#
+# Define tasks depending on each other and execute them.
+#
+# Copyright (c) 2015 Richard Klees <richard.klees@rwth-aachen.de>
+#
+# This software is licensed under The MIT License. You should have
+# received a copy of the LICENSE with the code.
+#
+
 try:
     from setuptools import setup
 except ImportError:
