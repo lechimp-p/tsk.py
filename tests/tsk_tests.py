d3067bc4b02e925fab2276abc643f3fa782f41cc
Richard Klees <richard.klees@rwth-aachen.de>
2016-01-27 21:48:32 +0100

    fixed year in (c) notice

--- tests/tsk_tests.py
+++ tests/tsk_tests.py
@@ -1,7 +1,7 @@
 #
 # Define tasks depending on each other and execute them.
 #
-# Copyright (c) 2015 Richard Klees <richard.klees@rwth-aachen.de>
+# Copyright (c) 2016 Richard Klees <richard.klees@rwth-aachen.de>
 #
 # This software is licensed under The MIT License. You should have
 # received a copy of the LICENSE with the code.
